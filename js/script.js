8ef646a0d1b1b2164f053fc113e7621773709517
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2024-12-17 14:39:45 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
 
   });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const counters = document.querySelectorAll(".counter-number");
+
+    counters.forEach((counter) => {
+      const target = +counter.getAttribute("data-target");
+      let count = 0;
+      const increment = target / 200; // السرعة
+
+      const updateCounter = () => {
+        if (count < target) {
+          count += increment;
+          counter.innerText = Math.ceil(count);
+          requestAnimationFrame(updateCounter);
+        } else {
+          counter.innerText = target;
+        }
+      };
+
+      updateCounter();
+    });
+  });
